390e525076ad0dacd2bd862b6337dde4932f499a
Remy Wang <remywang@protonmail.com>
2017-12-10 00:47:19 -0800

    iter 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,23 +1,27 @@
-{-# LANGUAGE BangPatterns, Strict #-}
+{-# LANGUAGE Strict #-}
 
 module Lib where
 
 import Debug.Trace
-import System.Environment
+-- import System.Environment
 
 data Nuc = A | C | T | G | N deriving Show
 type S = Bool
 
 someFunc :: IO ()
 someFunc = do
-  [arg] <- getArgs
-  let n = read arg
   fc <- readFile "NC_011297.fna"
   let _:ls = lines fc
   let dna = concat ls
   let os = map toNuc dna
   let v = foldl vnext v0 os
-  print (v n True)
+  let ((s1, ss1), (s2, ss2)) = v ((0, [False]), (0, [True]))
+  let ds1 = map (\b -> if b then '1' else '0') ss1
+  putStrLn ds1
+  print s1
+  let ds2 = map (\b -> if b then '1' else '0') ss2
+  putStrLn ds2
+  print s2
 
 toNuc :: Char -> Nuc
 toNuc 'A' = A
@@ -26,28 +30,30 @@ toNuc 'T' = T
 toNuc 'G' = G
 toNuc x = trace [x] N
 
-type V = Int -> S -> (Float, [S])
+-- type V = Int -> S -> (Float, [S])
+type V = ((Float, [S]), (Float, [S])) -> ((Float, [S]), (Float, [S]))
 
 v0 :: V
-v0 _ False = (log 0.996, [False])
-v0 _ True = (log 0.004, [True])
+v0 ((_, [False]), (_, [True])) = ((log 0.996, [False]), (log 0.004, [True]))
 
 vnext :: V -> Nuc -> V
-vnext vp o = v
-  where v 0 False = (log 0.996, [False])
-        v 0 True = (log 0.004, [True])
-        v i j = (log (e j o) * (max av1 av2), ss)
-          where av1 = log (a True j) + maxv
-                av2 = log (a False j) + maxv
-                (maxv, maxs) = maxf (vp (i-1))
-                ss = if av1 > av2
-                      then True : maxs
-                      else False : maxs
+vnext vp o = v . vp
+  where v ((s0f, s0s), (s1f, s1s)) = ((log (e True o) + max av10 av20, ss0), (log (e False o) + max av11 av21, ss1))
+          where av10 = log (a True False) + s1f
+                av20 = log (a False False) + s0f
+                ss0 = if av10 > av20
+                      then True : s1s
+                      else False : s0s
+                av11 = log (a True True) + s1f
+                av21 = log (a False True) + s0f
+                ss1 = if av11 > av21
+                      then True : s1s
+                      else False : s0s
 
-maxf :: (S -> (Float, [S])) -> (Float, [S])
-maxf f = if v1 > v2 then (v1, s1) else (v2, s2)
-  where (v1, s1) = f True
-        (v2, s2) = f False
+-- maxf :: (S -> (Float, [S])) -> (Float, [S])
+-- maxf f = if v1 > v2 then (v1, s1) else (v2, s2)
+--   where (v1, s1) = f True
+--         (v2, s2) = f False
 
 e :: S -> Nuc -> Float
 e True A = 0.291
