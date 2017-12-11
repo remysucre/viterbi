1557c9da277e4b5827171383b1a80b9547b63f8b
Remy Wang <remywang@uw.edu>
2017-12-10 18:32:58 -0800

    ok

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE Strict #-}
+-- {-# LANGUAGE Strict #-}
 
 module Lib where
 
 import Debug.Trace
+import Data.List
 
 data Nuc = A | C | T | G | N deriving (Show, Eq)
 data Cell = Cell {state :: S, score :: Float, chain :: [S]}
@@ -16,16 +17,9 @@ someFunc = do
   let _:ls = lines fc
       dna = concat ls
       os = map toNuc dna
-      v = foldl vsum v0 os
-      (Cell {score=n1, chain=ss1}, Cell {score=n0, chain=ss0}) =
-        v (Cell {state=False, score=0, chain=[False]}
-          ,Cell {state=True, score=0, chain=[True]})
-      ds1 = map (\b -> if b then '1' else '0') ss1
-      ds0 = map (\b -> if b then '1' else '0') ss0
-  putStrLn ds1
-  print n1
-  putStrLn ds0
-  print n0
+      (a, b, c, d, t) = iter os 10
+  print (a, b, c, d)
+  print (t True True, t True False, t False True, t False False)
 
 toNuc :: Char -> Nuc
 toNuc 'A' = A
@@ -35,34 +29,75 @@ toNuc 'G' = G
 toNuc x = trace [x] N
 
 v0 :: V
-v0 (Cell {chain=[False]}, Cell {chain=[True]}) =
-  (Cell {state=False, score=log 0.996, chain=[False]}
-  ,Cell {state=True, score=log 0.004, chain=[True]})
+v0 (Cell {chain=[]}, Cell {chain=[]}) =
+  (Cell {state=False, score=log 0.996, chain=[]}
+  ,Cell {state=True, score=log 0.004, chain=[]})
 v0 _ = undefined
 
-vsum :: V -> Nuc -> V
-vsum vp o = v . vp
+type E = S -> Nuc -> Float
+
+iter :: [Nuc] -> Int -> (Int, Int, [Int], [Int], T)
+iter _ 0 = (0, 0, [], [], a0)
+iter os n =
+  let (_, _, _, _, ap) = iter os (n-1)
+      v = foldl (vsum ap) v0 os
+      (Cell {score=n1, chain=ss1}, Cell {score=n0, chain=ss0}) =
+        v (Cell {state=False, score=0, chain=[]}
+          ,Cell {state=True, score=0, chain=[]})
+      ss = if n1 > n0 then ss1 else ss0
+      ins = count id ss
+      outs = count not ss
+      segs = group ss
+      cgs = map length (filter (elem True) segs)
+      ats = map length (filter (elem False) segs)
+      t = trans segs
+  in (ins, outs, cgs, ats, t)
+
+
+type T = S -> S -> Float
+
+trans :: [[S]] -> T
+trans segs = let cgs = length $ filter (elem True) segs
+                 ats = length $ filter (elem False) segs
+                 cgl = length . concat $ filter (elem True) segs
+                 atl = length . concat $ filter (elem False) segs
+                 t True True = 1 - t True False
+                 t True False = fromIntegral cgs / fromIntegral (cgl)
+                 t False False = 1 - t False True
+                 t False True = (fromIntegral ats-1) / fromIntegral (atl-1)
+             in t
+
+count :: (a -> Bool) -> [a] -> Int
+count p xs = foldl (\c x -> c + if p x then 1 else 0) 0 xs
+
+vsum :: T -> V -> Nuc -> V
+vsum a vp o = v . vp
   where v (Cell {state=s0, score=s0f, chain=s0s}
           ,Cell {state=s1, score=s1f, chain=s1s}) = (vnext s0, vnext s1)
           where vnext s =
-                  Cell {state=s, score=log (e s o) + max av1 av0, chain=ss}
+                  Cell {state=s, score=log (e0 s o)+ max av1 av0, chain=ss}
                   where av0 = log (a s0 s) + s0f
                         av1 = log (a s1 s) + s1f
-                        ss = if av1 > av0
-                             then s : s1s
-                             else s : s0s
+                        ss = s : if av1 > av0 then s1s else s0s
 
-e :: S -> Nuc -> Float
-e False n
-  | n==A || n==T = 0.291
-  | n==C || n==G = 0.209
-e True n
-  | n==A || n==T = 0.169
-  | n==C || n==G = 0.331
-e _ _  = 0.0
+e0 :: E
+e0 s n = let Just f = lookup (s, n) et0 in f
 
-a :: S -> S -> Float
-a False False = 0.999
-a False True = 0.001
-a True True = 0.99
-a True False = 0.01
+type ET = [((S, Nuc), Float)]
+
+et0 :: ET
+et0 = [ ((False, A), 0.291)
+      , ((False, T), 0.291)
+      , ((False, C), 0.209)
+      , ((False, G), 0.209)
+      , ((True, A), 0.169)
+      , ((True, T), 0.169)
+      , ((True, C), 0.331)
+      , ((True, G), 0.331)
+      ]
+
+a0 :: S -> S -> Float
+a0 False False = 0.999
+a0 False True = 0.001
+a0 True True = 0.99
+a0 True False = 0.01
